1c8f48f47caeb557b39cdcd7f212551d277dfa93
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-04-24 22:16:46 +0545

    query parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 import Loading from "./components/Loading"
+import { useState } from "react"
 
 
 function App() {
+  const [id, setId]= useState(1)
   const {data, isPending, refetch, error} = useQuery({
-    queryKey:['todos'],
-    queryFn: getTodos
+    queryKey:['todos', id],
+    queryFn:()=> getTodos(id),
   })
 
   if(error){
@@ -15,7 +17,7 @@ function App() {
     <>
     <h1>hello world</h1>
     <h1>{isPending? <Loading/>:JSON.stringify(data.slice(0,5))}</h1>
-    <button onClick={()=>refetch()}>refetch</button>
+    <button onClick={()=>setId((prev)=>prev+1)}>Increment id</button>
     
     
        
@@ -23,8 +25,8 @@ function App() {
   )
 }
 
-const getTodos= async ()=>{
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+const getTodos= async (id:number)=>{
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
   return await response.json()
 }
 
